804ef503d7995b0cb1a5c2255dd9d87bb78e7a0a
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-06-24 12:44:27 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -4,19 +4,36 @@ namespace paradigm_shift_csharp
 {
 class Checker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
+       static bool IsTemperatureOutOfRange(float temperature) {
+            return temperature < 0 || temperature > 45;
         }
-        return true;
-    }
+        
+        static bool IsSocOutOfRange(float soc) {
+            return soc < 20 || soc > 80;
+        }
+        
+        static bool IsChargeRateOutOfRange(float chargeRate) {
+            return chargeRate > 0.8;
+        }
+        
+        static bool BatteryIsOk(float temperature, float soc, float chargeRate) {
+            if (IsTemperatureOutOfRange(temperature)) {
+                Console.WriteLine("Temperature is out of range!");
+                return false;
+            }
+            
+            if (IsSocOutOfRange(soc)) {
+                Console.WriteLine("State of Charge is out of range!");
+                return false;
+            }
+            
+            if (IsChargeRateOutOfRange(chargeRate)) {
+                Console.WriteLine("Charge Rate is out of range!");
+                return false;
+            }
+            
+            return true;
+        }
 
     static void ExpectTrue(bool expression) {
         if(!expression) {

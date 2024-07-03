160e217d1f8f5ab49796b5c9dc5db7fc0403be42
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-03 12:29:04 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -40,13 +40,13 @@ class Checker
     static void ExpectTrue(bool expression) {
         if(!expression) {
             Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
+            // Environment.Exit(1);
         }
     }
     static void ExpectFalse(bool expression) {
         if(expression) {
             Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
+           // Environment.Exit(1);
         }
     }
     static int Main() {

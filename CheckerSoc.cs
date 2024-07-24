f90c047f1fa724bcbeb3e92d3e411784aa567b12
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-24 19:17:01 +0530

    Create CheckerSoc.cs

---
+++ CheckerSoc.cs
@@ -0,0 +1,22 @@
+partial class Checker
+{
+    static (bool, bool) IsSocOutOfRange(float soc)
+    {
+        return CheckRangeAndWarning(soc, SocUpperLimit, SocWarningTolerance);
+    }
+
+    static bool BatteryIsOkWithSoc(float soc)
+    {
+        var (isOutOfRange, isWarning) = IsSocOutOfRange(soc);
+        if (isOutOfRange)
+        {
+            Console.WriteLine("State of Charge is out of range!");
+            return false;
+        }
+        if (isWarning)
+        {
+            Console.WriteLine("Warning: State of Charge is approaching upper limit!");
+        }
+        return true;
+    }
+}

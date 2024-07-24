9e0befe16bdfa19513b4999aee0b0ed52b52a44c
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-24 19:17:32 +0530

    Create CheckerChargeRate.cs

---
+++ CheckerChargeRate.cs
@@ -0,0 +1,22 @@
+partial class Checker
+{
+    static (bool, bool) IsChargeRateOutOfRange(float chargeRate)
+    {
+        return CheckRangeAndWarning(chargeRate, ChargeRateUpperLimit, ChargeRateWarningTolerance);
+    }
+
+    static bool BatteryIsOkWithChargeRate(float chargeRate)
+    {
+        var (isOutOfRange, isWarning) = IsChargeRateOutOfRange(chargeRate);
+        if (isOutOfRange)
+        {
+            Console.WriteLine("Charge Rate is out of range!");
+            return false;
+        }
+        if (isWarning)
+        {
+            Console.WriteLine("Warning: Charge Rate is approaching upper limit!");
+        }
+        return true;
+    }
+}

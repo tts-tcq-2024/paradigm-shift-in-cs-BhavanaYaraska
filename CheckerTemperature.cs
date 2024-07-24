07e8ec0d05d9bb6846f149202c67989dfed01215
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-24 19:16:02 +0530

    Create CheckerTemperature.cs

---
+++ CheckerTemperature.cs
@@ -0,0 +1,22 @@
+partial class Checker
+{
+    static (bool, bool) IsTemperatureOutOfRange(float temperature)
+    {
+        return CheckRangeAndWarning(temperature, TemperatureUpperLimit, TemperatureWarningTolerance);
+    }
+
+    static bool BatteryIsOkWithTemperature(float temperature)
+    {
+        var (isOutOfRange, isWarning) = IsTemperatureOutOfRange(temperature);
+        if (isOutOfRange)
+        {
+            Console.WriteLine("Temperature is out of range!");
+            return false;
+        }
+        if (isWarning)
+        {
+            Console.WriteLine("Warning: Temperature is approaching upper limit!");
+        }
+        return true;
+    }
+}

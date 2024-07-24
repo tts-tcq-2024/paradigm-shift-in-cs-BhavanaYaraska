f8f1f300abad47a88765a3ac11889003c4b72b2e
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-24 19:18:15 +0530

    Create CheckerUtilities.cs

---
+++ CheckerUtilities.cs
@@ -0,0 +1,27 @@
+partial class Checker
+{
+    static (bool, bool) CheckRangeAndWarning(float value, float upperLimit, float warningTolerance)
+    {
+        bool isOutOfRange = value < 0 || value > upperLimit;
+        bool isWarning = value >= upperLimit - warningTolerance && value <= upperLimit;
+        return (isOutOfRange, isWarning);
+    }
+
+    static void ExpectTrue(bool expression)
+    {
+        if (!expression)
+        {
+            Console.WriteLine("Expected true, but got false");
+            // Environment.Exit(1);
+        }
+    }
+
+    static void ExpectFalse(bool expression)
+    {
+        if (expression)
+        {
+            Console.WriteLine("Expected false, but got true");
+            // Environment.Exit(1);
+        }
+    }
+}

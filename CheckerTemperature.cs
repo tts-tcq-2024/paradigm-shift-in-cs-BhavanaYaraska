b20a6159a9b2d90986ff610b5d759ede4d386049
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-24 19:27:36 +0530

    Update CheckerTemperature.cs

--- CheckerTemperature.cs
+++ CheckerTemperature.cs
@@ -1,3 +1,5 @@
+namespace paradigm_shift_csharp
+{
 partial class Checker
 {
     static (bool, bool) IsTemperatureOutOfRange(float temperature)
@@ -19,4 +21,5 @@ partial class Checker
         }
         return true;
     }
+}
 }

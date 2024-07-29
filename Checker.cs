830d228b0745470d57c67829ea81371ce4bd214d
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-30 00:43:42 +0530

    Update Checker.cs

--- Checker.cs
+++ Checker.cs
@@ -1,28 +1,84 @@
-using System;
 namespace paradigm_shift_csharp
 {
-public class Checker
-{
-    static readonly float TemperatureUpperLimit = 45;
-    static readonly float SocUpperLimit = 80;
-    static readonly float ChargeRateUpperLimit = 0.8f;
-
-    // Define warning tolerance as 5% of the upper limit
-    static readonly float TemperatureWarningTolerance = TemperatureUpperLimit * 0.05f;
-    static readonly float SocWarningTolerance = SocUpperLimit * 0.05f;
-    static readonly float ChargeRateWarningTolerance = ChargeRateUpperLimit * 0.05f;
-
-    
-    static int Main()
+    public class Checker
     {
-        ExpectTrue(BatteryIsOkWithTemperature(25));
-        ExpectTrue(BatteryIsOkWithSoc(70));
-        ExpectTrue(BatteryIsOkWithChargeRate(0.7f));
-        ExpectFalse(BatteryIsOkWithTemperature(50));
-        ExpectFalse(BatteryIsOkWithSoc(85));
-        ExpectFalse(BatteryIsOkWithChargeRate(0.0f));
-        Console.WriteLine("All ok");
-        return 0;
+        // Constants
+        private const float TemperatureUpperLimit = 45.0f;
+        private const float TemperatureWarningTolerance = 5.0f;
+        private const float SocUpperLimit = 80.0f;
+        private const float SocWarningTolerance = 10.0f;
+        private const float ChargeRateUpperLimit = 2.0f;
+        private const float ChargeRateWarningTolerance = 0.5f;
+
+        // Utility method
+        public static (bool isOutOfRange, bool isWarning) CheckRangeAndWarning(float value, float upperLimit, float warningTolerance)
+        {
+            bool isOutOfRange = value < 0 || value > upperLimit;
+            bool isWarning = value >= upperLimit - warningTolerance && value <= upperLimit;
+            return (isOutOfRange, isWarning);
+        }
+
+        // Temperature methods
+        public static (bool isOutOfRange, bool isWarning) IsTemperatureOutOfRange(float temperature)
+        {
+            return CheckRangeAndWarning(temperature, TemperatureUpperLimit, TemperatureWarningTolerance);
+        }
+
+        public static bool BatteryIsOkWithTemperature(float temperature)
+        {
+            var (isOutOfRange, isWarning) = IsTemperatureOutOfRange(temperature);
+            if (isOutOfRange)
+            {
+                Console.WriteLine("Temperature is out of range!");
+                return false;
+            }
+            if (isWarning)
+            {
+                Console.WriteLine("Warning: Temperature is approaching upper limit!");
+            }
+            return true;
+        }
+
+        // SOC methods
+        public static (bool isOutOfRange, bool isWarning) IsSocOutOfRange(float soc)
+        {
+            return CheckRangeAndWarning(soc, SocUpperLimit, SocWarningTolerance);
+        }
+
+        public static bool BatteryIsOkWithSoc(float soc)
+        {
+            var (isOutOfRange, isWarning) = IsSocOutOfRange(soc);
+            if (isOutOfRange)
+            {
+                Console.WriteLine("SOC is out of range!");
+                return false;
+            }
+            if (isWarning)
+            {
+                Console.WriteLine("Warning: SOC is approaching upper limit!");
+            }
+            return true;
+        }
+
+        // Charge Rate methods
+        public static (bool isOutOfRange, bool isWarning) IsChargeRateOutOfRange(float chargeRate)
+        {
+            return CheckRangeAndWarning(chargeRate, ChargeRateUpperLimit, ChargeRateWarningTolerance);
+        }
+
+        public static bool BatteryIsOkWithChargeRate(float chargeRate)
+        {
+            var (isOutOfRange, isWarning) = IsChargeRateOutOfRange(chargeRate);
+            if (isOutOfRange)
+            {
+                Console.WriteLine("Charge rate is out of range!");
+                return false;
+            }
+            if (isWarning)
+            {
+                Console.WriteLine("Warning: Charge rate is approaching upper limit!");
+            }
+            return true;
+        }
     }
-}
 }

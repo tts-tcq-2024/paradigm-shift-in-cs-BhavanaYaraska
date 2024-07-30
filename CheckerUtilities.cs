c75e81f509529a925796a2e21c825468ebee530a
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-30 21:17:55 +0530

    Update CheckerUtilities.cs

--- CheckerUtilities.cs
+++ CheckerUtilities.cs
@@ -2,7 +2,7 @@ namespace paradigm_shift_csharp
 {
 public static class CheckerUtilities
 {
-    static (bool, bool) CheckRangeAndWarning(float value, float upperLimit, float warningTolerance)
+    public static (bool, bool) CheckRangeAndWarning(float value, float upperLimit, float warningTolerance)
     {
         bool isOutOfRange = value < 0 || value > upperLimit;
         bool isWarning = value >= upperLimit - warningTolerance && value <= upperLimit;

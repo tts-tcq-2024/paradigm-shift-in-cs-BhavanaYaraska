569e6734b71cb442e53e3089ab5a507b99785909
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-30 22:33:24 +0530

    Update CheckerUtilities.cs

--- CheckerUtilities.cs
+++ CheckerUtilities.cs
@@ -8,23 +8,5 @@ public static class CheckerUtilities
         bool isWarning = value >= upperLimit - warningTolerance && value <= upperLimit;
         return (isOutOfRange, isWarning);
     }
-
-    static void ExpectTrue(bool expression)
-    {
-        if (!expression)
-        {
-            Console.WriteLine("Expected true, but got false");
-            // Environment.Exit(1);
-        }
-    }
-
-    static void ExpectFalse(bool expression)
-    {
-        if (expression)
-        {
-            Console.WriteLine("Expected false, but got true");
-            // Environment.Exit(1);
-        }
-    }
 }
 }

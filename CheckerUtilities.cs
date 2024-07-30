e7f44a72da4513dcb8c919157b2f176dce2deb48
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-30 20:46:05 +0530

    Update CheckerUtilities.cs

--- CheckerUtilities.cs
+++ CheckerUtilities.cs
@@ -1,31 +1,31 @@
-// namespace paradigm_shift_csharp
-// {
-// public class CheckerUtilities
-// {
-//     static (bool, bool) CheckRangeAndWarning(float value, float upperLimit, float warningTolerance)
-//     {
-//         bool isOutOfRange = value < 0 || value > upperLimit;
-//         bool isWarning = value >= upperLimit - warningTolerance && value <= upperLimit;
-//         return (isOutOfRange, isWarning);
-//     }
+namespace paradigm_shift_csharp
+{
+public class CheckerUtilities
+{
+    static (bool, bool) CheckRangeAndWarning(float value, float upperLimit, float warningTolerance)
+    {
+        bool isOutOfRange = value < 0 || value > upperLimit;
+        bool isWarning = value >= upperLimit - warningTolerance && value <= upperLimit;
+        return (isOutOfRange, isWarning);
+    }
 
-//     static void ExpectTrue(bool expression)
-//     {
-//         if (!expression)
-//         {
-//             Console.WriteLine("Expected true, but got false");
-//             // Environment.Exit(1);
-//         }
-//     }
+    static void ExpectTrue(bool expression)
+    {
+        if (!expression)
+        {
+            Console.WriteLine("Expected true, but got false");
+            // Environment.Exit(1);
+        }
+    }
 
-//     static void ExpectFalse(bool expression)
-//     {
-//         if (expression)
-//         {
-//             Console.WriteLine("Expected false, but got true");
-//             // Environment.Exit(1);
-//         }
-//     }
-// }
+    static void ExpectFalse(bool expression)
+    {
+        if (expression)
+        {
+            Console.WriteLine("Expected false, but got true");
+            // Environment.Exit(1);
+        }
+    }
+}
 
-// }
+}

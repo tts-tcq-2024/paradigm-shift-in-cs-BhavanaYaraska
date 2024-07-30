11a0cee06fb12e1ca7fe94f0eefbef1a44aca702
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-30 20:40:14 +0530

    Update and rename 1 to CheckerChargeRate.cs

---
+++ CheckerChargeRate.cs
@@ -0,0 +1,24 @@
+namespace paradigm_shift_csharp
+{
+partial class CheckerChargeRate
+{
+    static (bool, bool) IsChargeRateOutOfRange(float chargeRate)
+    {
+        return CheckRangeAndWarning(chargeRate, ChargeRateUpperLimit, ChargeRateWarningTolerance);
+    }
+    static bool BatteryIsOkWithChargeRate(float chargeRate)
+    {
+        var (isOutOfRange, isWarning) = IsChargeRateOutOfRange(chargeRate);
+        if (isOutOfRange)
+        {
+            Console.WriteLine("Charge Rate is out of range!");
+            return false;
+        }
+        if (isWarning)
+        {
+            Console.WriteLine("Warning: Charge Rate is approaching upper limit!");
+        }
+        return true;
+    }
+}
+}
